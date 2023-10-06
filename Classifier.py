32d5a32dcbdf492fcf15d131c34e51263b9ebf9c
YASSERZID <97595866+YASSERZID@users.noreply.github.com>
2023-10-06 09:42:39 +0200

    -TP2

--- Classifier.py
+++ Classifier.py
@@ -1,55 +1,85 @@
-import string
 import pandas as pd
+import matplotlib.pyplot as plt
+import string
 
-POS_LABEL = [
-    "NOUN",
-    "ADP",
-    "DET",
-    "PUNCT",
-    "VERB",
-    "PROPN",
-    "ADJ",
-    "PRON",
-    "ADV",
-    "AUX",
-    "CCONJ",
-    "NUM",
-    "X",
-    "SCONJ",
-    "SYM",
-    "INTJ"
-]
+class ConLLUParser:
 
-class NaiveClassifier:
-
-    def __init__(self) -> None:
-        pass
-
-    def predict(self, data:pd.DataFrame):
-
-        assert 'FORM' in data.columns, 'Column FORM is missing from data'
+    def __init__(self, path, verbose=False) -> None:
+        # open .conllu file and split each line and remove the '\n' character at the end
+        with open(path) as f:
+            raw_lines = f.read().splitlines()
+            #splitting lines from the conlu parser 
         
-        def naive_rules(word):
-            if word.endswith('s'):
-                return 'VERB'
-            elif word[0].isupper():
-                return 'PROPN'
-            elif word.isdigit():
-                return 'NUM'
-            elif word.lower().startswith('qu'):
-                return 'SCONJ'
-            elif all(i in string.punctuation for i in word):
-                return 'PUNCT'
+        # retrieve the name of the columns
+        self.col_names = [
+            'ID', 'FORM', 'LEMMA', 'UPOS', 'XPOS', 'FEATS', 'HEAD', 'DEPREL', 'DEPS', 'MISC'
+        ]
+        
+        # all_words stores each word of the corpus, identified by sent_id (id of the sentence in the corpus) and the ID (position of the word in the sentence)
+        # all_contractions stores the contractions, e.g. 'du' -> 'de le' ; 'didn't' -> 'did n't'
+        # all_sentences stores each sentence of the corpus, identified by sent_id and computes the length of the sentence
+        all_words, all_contractions, all_sentences = [], [], []
+        
+        # temporary variables
+        temp, count, n_exceptions = [], 0, 0
+
+        # treat the corpus line by line using pop function
+        while len(raw_lines) > 0:
+            
+            line = raw_lines.pop(0)
+            if len(line) > 0:
+                splits = line.split('\t')
+            
+            # the sentences always start by a line giving the sent_id "# sent_id = ..."
+            if line.startswith('# sent_id = '):
+                id = line.replace('# sent_id = ', '')
+                temp.append(id)
+                
+            
+            # then another one gives the sentence "# text = ..."
+            elif line.startswith('# text = '):
+                text = line.replace('# text = ', '')
+                temp.append(text)
+            
+            # a sentence is ended by a blank line
+            elif line == '':
+                if count > 0:
+                    temp.append(count)
+                    all_sentences.append(temp)
+                count = 0
+                temp = []
+
+            # print all lines that don't start by a digit as their are not useful
+            # count them for later sanity checks
+            elif not line[0].isdigit():
+                if verbose:
+                    print(f'IS OMITTED: {line}')
+                n_exceptions += 1
+
+            # the lines that start by a number contain the words and their characteristics
+            elif splits[0].isdigit():
+                assert len(splits) == len(self.col_names)
+                all_words.append([id] + splits)
+                count += 1
+
+            # if the start of a line contains a dash, then it represents a contradiction
+            elif '-' in splits[0] or '.' in splits[0]:
+                all_contractions.append([id] + splits[:2])
+            
             else:
-                return 'NOUN'
-        return data.FORM.apply(naive_rules).tolist()
+                raise ValueError(f'Cannot handle this line: {line}')
+            
+
+        self.sentences = pd.DataFrame(all_sentences, columns=['sent_id', 'text', 'length'])
+        self.words = pd.DataFrame(all_words, columns=['sent_id'] + self.col_names)
+        self.contradictions = pd.DataFrame(all_contractions, columns=['sent_id', 'ID', 'FORM'])
+            
 
 
-class RandomClassifier:
+if __name__=="__main__":
 
-    def __init__(self, random_seed=0) -> None:
-        self.random_seed = random_seed
-        self.label = POS_LABEL
-    
-    def predict(self, data):
-        pass
+    verbose = False
+    fr_train = ConLLUParser('fr_gsd-ud-train.conllu', verbose)
+    fr_dev = ConLLUParser('fr_gsd-ud-dev.conllu', verbose)
+    fr_test = ConLLUParser('fr_gsd-ud-test.conllu', verbose)
+
---
+++ differentClassifiers.py
@@ -0,0 +1,145 @@
+import string
+import pandas as pd
+import random
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+POS_LABEL = [
+    "NOUN", "ADP", "DET", "PUNCT", "VERB", "PROPN", "ADJ", "PRON", "ADV",
+    "AUX", "CCONJ", "NUM", "X", "SCONJ", "SYM", "INTJ"
+]
+
+
+
+
+
+class NaiveClassifier:
+    
+
+    def __init__(self) -> None:
+        pass
+
+    def predict(self, data: pd.DataFrame):
+
+        assert 'FORM' in data.columns, 'Column FORM is missing from data'
+        
+        def naive_rules(word):
+            word = word.lower()  # Convert word to lowercase for consistent rule checking
+            
+            if word.endswith(('tion', 'sion', 'ment')) or word in ['ville', 'année', 'pays', 'nom', 'jour', 'mois', 'homme', 'femme', 'vie', 'enfant']:
+                return 'NOUN'
+
+            if word.endswith(('er', 'ir', 're')) or word in ['être', 'avoir', 'est', 'était', 'été', 'fut', 'étant']:
+                return 'VERB'
+            if word in ['être', 'avoir', 'est', 'était', 'été', 'fut', 'étant']:
+                return 'AUX'
+            if word.endswith(('eux', 'euse')) or word in ['premier', 'première', 'français', 'française', 'nouveau', 'nouvelle', 'grand', 'grande', 'petit', 'petite']:
+                return 'ADJ'
+            if word.endswith('ment') or word in ['très', 'bien', 'aussi', 'toujours', 'surtout', 'encore', 'trop', 'jamais', 'vraiment']:
+                return 'ADV'
+            if word in ['le', 'la', 'les', 'l', 'un', 'une', 'des']:
+                return 'DET'
+            if word in ['il', 'elle', 'nous', 'vous', 'ils', 'elles', 'lui', 'on', 'je', 'me', 'ma', 'mon', 'leur', 'leurs']:
+                return 'PRON'
+            if word in ['et', 'ou', 'mais', 'donc']:
+                return 'CCONJ'
+            if word in ['que', 'quand', 'si', 'lorsque']:
+                return 'SCONJ'
+            if word in ['de', 'à', 'en', 'sur', 'avec', 'dans', 'par', 'pour', 'sans', 'sous', 'contre', 'après', 'avant', 'vers', 'chez']:
+                return 'ADP'
+            if word.isdigit() or word in ['un', 'deux', 'trois', 'deuxième', 'troisième']:
+                return 'NUM'
+            if word[0].isupper() and word not in ['et', 'ou', 'mais', 'donc']:
+                return 'PROPN'
+            if word in [',', '.', '(', ')', ':', ';', '«', '»', '!', '?']:
+                return 'PUNCT'
+            if word in ['%', '€']:
+                return 'SYM'
+            if word in ['oh', 'ah', 'eh']:
+                return 'INTJ'
+            return 'NOUN'
+        
+        return data.FORM.apply(naive_rules).tolist()
+    def evaluate(self, y_true, y_pred):
+        return accuracy_score(y_true, y_pred)
+
+class RandomClassifier:
+
+    def __init__(self, random_seed=0) -> None:
+        self.random_seed = random_seed
+        self.label = POS_LABEL
+    
+    def predict(self, data: pd.DataFrame):
+        random.seed(self.random_seed)
+        return [random.choice(self.label) for _ in range(len(data))]
+    def evaluate(self, y_true, y_pred):
+        return accuracy_score(y_true, y_pred)
+
+class StratifiedClassifier: 
+    def __init__(self) -> None :
+        pass
+    def predict(self, data: pd.DataFrame):
+        valuecounts= data['UPOS'].value_counts(normalize=True).to_dict()
+        probabilities = list(valuecounts.values())
+        labels = list(valuecounts.keys())
+        N=len(data)
+        
+        return np.random.choice(POS_LABEL, size=N, p=probabilities)
+    def evaluate(self, data: pd.DataFrame):
+        y_true = data['UPOS'].tolist()
+        y_pred = self.predict(data)
+        
+        assert len(y_true) == len(y_pred), "y_true and y_pred do not have the same length"
+        
+        metrics = {}
+        metrics['accuracy'] = accuracy_score(y_true, y_pred)
+        return metrics
+
+class MostCommonPOSClassifier:
+    def __init__(self) -> None:
+        self.most_common_pos = None
+
+    def predict(self, data: pd.DataFrame):
+        self.most_common_pos = data['UPOS'].value_counts().idxmax()    
+        return [self.most_common_pos] * len(data)
+    
+    def evaluate(self, data: pd.DataFrame):
+        y_true = data['UPOS'].tolist()
+        y_pred = self.predict(data)
+        assert len(y_true) == len(y_pred), "y_true and y_pred do not have the same length"
+        metrics = {}
+        metrics['accuracy'] = accuracy_score(y_true, y_pred)
+        return metrics
+
+class WordDistributedRandomClassifier:
+    def __init__(self):
+        self.word_distributions = {}
+        self.global_distribution = None
+
+    def fit(self, data: pd.DataFrame):
+        #on train_data
+        # what's the POS distribution on each word 
+        self.global_distribution = data['UPOS'].value_counts(normalize=True).to_dict()
+        for word, group in data.groupby('FORM'):
+            self.word_distributions[word] = group['UPOS'].value_counts(normalize=True).to_dict()
+
+    def predict(self, data: pd.DataFrame):
+        predictions = []
+        for word in data['FORM']:
+            if word in self.word_distributions:
+                pos_tags = list(self.word_distributions[word].keys())
+                probabilities = list(self.word_distributions[word].values())
+                predictions.append(np.random.choice(pos_tags, p=probabilities))
+            else:
+                pos_tags = list(self.global_distribution.keys())
+                probabilities = list(self.global_distribution.values())
+                predictions.append(np.random.choice(pos_tags, p=probabilities))
+        return predictions
+
+    def evaluate(self, data: pd.DataFrame):
+        y_true = data['UPOS'].tolist()
+        y_pred = self.predict(data)
+        return accuracy_score(y_true, y_pred)
+
+
+
